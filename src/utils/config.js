08e829466a1341fdb1f4902bb49e6dd697243f81
MhtChawla <chawla.mohi@gmail.com>
2022-11-02 16:05:45 +0530

    This commit will update config file

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
 export const APP_NAME = 'BaseKit'; //give your app name here
 export const BASE_URL =
-  'https://cv2vgemb5d.execute-api.ap-south-1.amazonaws.com/integration'; //add your base link here
+  'https://d.execute-api.ap-south-1.amazonaws.com/int'; //add your base link here
