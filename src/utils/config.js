604ed89b1d89a0a6c0f8a88467a520176858b8a4
MhtChawla <chawla.mohi@gmail.com>
2022-11-04 13:22:17 +0530

    This commit ill update baseUrl in config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
 export const APP_NAME = 'BaseKit'; //give your app name here
 export const BASE_URL =
-  'https://hub.dummyapis.com/delay?seconds='; //add your base link here
+  'https://hub.dummyapis.com'; //add your base link here
