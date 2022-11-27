1755ceef44cbe581fdb4be34045ef66ada936e34
Mohit Chawla <66668781+MhtChawla@users.noreply.github.com>
2022-11-27 19:08:59 +0530

    Update StatusBar.js

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -1,7 +1,7 @@
 import {StatusBar} from 'react-native';
 import React from 'react';
 
-const CustomStatusBar = ({backgroundColor, barStyle = 'dark-content'}) => {
+const CustomStatusBar = ({backgroundColor, barStyle}) => {
   return (
     <StatusBar
       animated={true}
